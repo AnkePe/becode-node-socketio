d7d2436d464657d3ef67283c0240fecf37201252
Anke Peeraer <peeraeranke@gmail.com>
2019-02-26 17:04:00 +0100

    onechat

--- app.js
+++ app.js
@@ -22,7 +22,52 @@ server = app.listen( port, () => {
 //socket io gebruiken
 const io = require('socket.io')(server)
 
+//lijst van usernames maken
+const allUsernames = [] //lege array
+
 //listen on every connection
 io.on('connection', (socket) => {
     console.log(`New user connected`)
+
+    //default username
+    socket.username = "Anoniem"
+
+    //listen on change_username
+    socket.on('change_username', (data, callback) => {
+        socket.username = data.username
+        if(allUsernames.indexOf(socket.username != -1)) {
+            callback(false)
+        } else {
+            callback(true)
+            allUsernames.push(socket.username)
+            updateAllUsernames()
+        }
+    })
+
+    //function updateAllUsernames
+    const updateAllUsernames = () => {
+        io.sockets.emit('allUsernames', allUsernames)
+    }
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message => we call the sockets property of io = all sockets connected
+        io.sockets.emit('new_message', {message : data.message, username : socket.username})
+    })
+    //listen on typing
+    socket.on('typing', (data) => {
+        //use broadcast to send to every socket except the one that started typing
+        socket.broadcast.emit('typing', {username : socket.username})   
+    })
+
+    //listen on disconnect
+    socket.on('disconnect', (data) => {
+        // if(!socket.username) {
+        //     console.log('disconnected')
+        //     return
+        // }
+        allUsernames.splice(allUsernames.indexOf(socket.username), 1)
+        updateAllUsernames()
+
+    })
+
 })
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,44 @@
-$
+// document ready
+$(() => {
+    //make connection
+    const socket = io.connect('http://localhost:3000')
+
+    //buttons and inputs
+	const message = $("#message")
+	const username = $("#username")
+	const send_message = $("#send_message") //button
+	const send_username = $("#send_username")   //button
+	const chatroom = $("#chatroom")
+    const feedback = $("#feedback")
+
+    //Emit message
+	send_message.click(() => {
+		socket.emit('new_message', {message : message.val()})
+    })
+    
+    //Listen on new_message
+    socket.on('new_message', (data) => {
+        console.log(data)
+        feedback.html('')   //... is typing leegmaken
+        message.val('') //het invoerveld voor messages leegmaken
+        chatroom.append(`<p class='message'>${data.username}: ${data.message}</p>`)
+    })
+
+    //Emit username
+    send_username.click(() => {
+        console.log(username.val())
+        socket.emit('change_username', {username : username.val()})
+    })
+    
+    //Emit typing
+    message.on('keypress', () => {
+        socket.emit('typing')
+    })
+
+    //Listen on typing
+    socket.on('typing', (data) => {
+        feedback.html(`<p><i>${data.username} is typing a message...</i></p>`)
+
+    })
+
+})
